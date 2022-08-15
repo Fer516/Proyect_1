8b048726bc76923294eb6b313f56c1c6c18ea312
Fer516 <110191651+Fer516@users.noreply.github.com>
2022-08-14 22:26:12 -0600

    Add files via upload

---
+++ Ejercicio_2/Ejercicio_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double base1, altura, area, perimetro;
+            Console.Write("Base: ");
+            base1 = double.Parse(Console.ReadLine());
+            Console.Write("Altura: ");
+            altura = double.Parse(Console.ReadLine());
+            area = base1 * altura / 2;
+            perimetro = base1 * 3;
+            Console.WriteLine("Area = {0}", area);
+            Console.WriteLine("Perimetro = {0}", perimetro);
+            Console.ReadKey();
+        }
+    }
+}
