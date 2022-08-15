50a807de562a2cbf4918c8603e7a7f3777732511
Fer516 <110191651+Fer516@users.noreply.github.com>
2022-08-14 22:25:48 -0600

    Add files via upload

---
+++ Ejercicio_1/Ejercicio_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horas, minutos,segundos;
+            Console.Write("Ingresa el valor de horas: ");
+            horas = int.Parse(Console.ReadLine());
+            minutos = horas * 60;
+            segundos = horas * 3600;
+            Console.WriteLine("Valor de minutos: " + minutos);
+            Console.WriteLine();
+            Console.WriteLine("Valor de minutos: " + segundos);
+            Console.WriteLine();
+            Console.Write("Presiona una tecla para terminar . . . ");
+            Console.ReadKey();
+        }
+    }
+}
